a893316e63c0e2a05e2e501d03fcb554b21f5443
Kritika Surana <suranakritika@gmail.com>
2020-01-15 10:26:41 +0530

    Add files via upload
    Updated PID.h, PID.cpp

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,30 +1,45 @@
 #include "PID.h"
 
-/**
- * TODO: Complete the PID class. You may add any additional desired functions.
- */
+#include <algorithm>
+// using namespace std;
+
+/*
+* TODO: Complete the PID class.
+*/
 
 PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp_, double Ki_, double Kd_) {
-  /**
-   * TODO: Initialize PID coefficients (and errors, if needed)
-   */
+void PID::Init(double Kp, double Ki, double Kd) {
+  PID::Kp = Kp;
+  PID::Ki = Ki;
+  PID::Kd = Kd;
+
+  p_error = 0.0;
+  i_error = 0.0;
+  d_error = 0.0;
+
+  // Previous cte.
+  prev_cte = 0.0;
 
 }
 
 void PID::UpdateError(double cte) {
-  /**
-   * TODO: Update PID errors based on cte.
-   */
+  // Proportional error.
+  p_error = cte;
+
+  // Integral error.
+  i_error += cte;
+
+  // Diferential error.
+  d_error = cte - prev_cte;
+  prev_cte = cte;
 
 }
 
 double PID::TotalError() {
-  /**
-   * TODO: Calculate and return the total error
-   */
-  return 0.0;  // TODO: Add your total error calc here!
+//   return p_error * Kp + i_error * Ki + d_error * Kd;
+  return (-Kp * p_error) - (Ki * i_error) - (Kd * d_error);
 }
+
